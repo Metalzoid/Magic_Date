af04ed080eeeec3a5b3d45cb30b0a310ea2b8afb
Florian Gagnaire <gagnaire.flo@gmail.com>
2024-05-28 15:41:29 +0200

    Navbar + users roles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_27_150010) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_28_124943) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,6 +47,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_27_150010) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

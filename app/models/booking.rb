a761234cb3a59ec19f1d377fc471f7d965fe9cba
Florian Gagnaire <gagnaire.flo@gmail.com>
2024-05-29 12:47:26 +0200

    Validation valids

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,9 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :offer
+
+  validates :total_price, presence: true, numericality: { only_integer: true }
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :comment, presence: true
 end

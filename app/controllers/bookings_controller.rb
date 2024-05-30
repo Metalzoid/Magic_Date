c3df080db744d9ac6b56cc335b9a02964d1d2089
Nicolas Thouvenot <prodorbitale@gmail.com>
2024-05-30 11:07:58 +0200

    Before merge

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,9 +3,17 @@ class BookingsController < ApplicationController
   end
 
   def new
+    @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def update

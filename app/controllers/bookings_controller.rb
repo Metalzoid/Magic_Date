9ca4d2e3e5547333558c8810444526603e2daec7
Nicolas Thouvenot <prodorbitale@gmail.com>
2024-05-30 11:37:06 +0200

    Form done without js

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,11 +4,15 @@ class BookingsController < ApplicationController
 
   def new
     @booking = Booking.new
+    @offer = Offer.find(params[:offer_id])
   end
 
   def create
+    @offer = Offer.find(params[:offer_id])
     @booking = Booking.new(booking_params)
     @booking.user = current_user
+    @booking.offer = @offer
+    @booking.status = "Pending"
     if @booking.save
       redirect_to booking_path(@booking)
     else
@@ -20,5 +24,15 @@ class BookingsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:comment, :total_price, :user_id, :offer_id, :status, :start_date, :end_date)
+  end
+
+  def set_offer
+    @offer = Offer.find(params[:offer_id])
   end
 end

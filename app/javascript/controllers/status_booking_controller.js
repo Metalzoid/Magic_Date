f11d67b2d22408d76a94aba72715ef19f11d9139
Florian Gagnaire <gagnaire.flo@gmail.com>
2024-05-31 10:57:03 +0200

    Update finish

--- app/javascript/controllers/status_booking_controller.js
+++ app/javascript/controllers/status_booking_controller.js
@@ -4,6 +4,6 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["form"];
   status() {
-    console.log(this.formTarget.querySelector("#booking_status").value)
+    this.formTarget.submit()
   }
 }
